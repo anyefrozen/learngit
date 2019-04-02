a69601b02690f44c85dd74cdd910ff01925e584f
anyefrozen <1024900614@qq.com>
2019-04-02 15:44:23 +0800

    cs

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+
+	set_time_limit(5);
+	
+
+	$url = $argv[1];
+	$s = preg_replace("/\/\d{2,5}_\d{2,5}", '/size/', $url);
+
+	var_dump($s);
+
+	if (is_null($s)) {
+		echo 'error_log';
+		exit;
+	}
+
+	echo "$s";
+    
+?>
