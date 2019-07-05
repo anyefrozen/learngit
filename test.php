ad53d9598ba4c6bf4c58997b105a67422edc9259
anyefrozen <1024900614@qq.com>
2019-07-05 18:35:14 +0800

    test.php

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 
-	set_time_limit(5);
+	/*set_time_limit(5);
 	
 	$url = $argv[1];
 	$s = preg_replace("/\/\d{2,5}_\d{2,5}", '/size/', $url);
@@ -12,6 +12,20 @@
 		exit;
 	}
 
-	echo "$s";
+	echo "$s";*/
+
+	class person{
+		public $name;
+		public $gender;
+
+		public function say(){
+			echo $this->name." is ".$this->gender;
+		}
+	}
+
+	$student = new person();
+	$student->name = 'Tom';
+	$student->gender = 'male';
+	$student->say();
     
 ?>
